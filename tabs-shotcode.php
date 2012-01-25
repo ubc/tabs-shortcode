605ea9ea08e138a47fa6e0efe4deab4f21c5159b
Enej Bajgoric <enej.bajgoric@gmail.com>
2012-01-25 14:48:09 -0800

    updated the tabs-shortcode plugin

--- tabs-shotcode.php
+++ tabs-shotcode.php
@@ -33,7 +33,7 @@ function olt_display_shortcode_tab($atts,$content)
 	<?php
 	else:
 	?>
-		<span style="color:red">Please enter a title attribite like [tab title="title name"]tab content[tab]</span>
+		<span style="color:red">Please enter a title attribute like [tab title="title name"]tab content[tab]</span>
 		<?php 	
 	endif;
 	$olt_tab_shortcode_count++;
@@ -41,7 +41,7 @@ function olt_display_shortcode_tab($atts,$content)
 }
 
 function olt_display_shortcode_tabs($attr,$content)
-{	
+{
 	// wordpress function 
 	$pattern = get_shortcode_regex();
 	global $olt_tab_shortcode_count,$post;
@@ -70,8 +70,7 @@ function olt_display_shortcode_tabs($attr,$content)
 						<?php
 						elseif( $post->post_title ):
 							?><li <?php if( $attr['class']): ?> class='<?php echo $attr['class'];?>' <?php endif; ?>
-							><a href="#<?php echo ereg_replace("[^A-Za-z0-9]", "", $post->post_title)."-".$tab_count; ?>"><?php echo $post->post_title; ?></a></li>
-						<?php
+							><a href="#<?php echo ereg_replace("[^A-Za-z0-9]", "", $post->post_title)."-".$tab_count; ?>"><?php echo $post->post_title; ?></a></li><?php
 						
 						endif;
 						 $tab_count++;
@@ -79,10 +78,8 @@ function olt_display_shortcode_tabs($attr,$content)
 					$count++;
 				endforeach;
 				
-			?>
-		</ul>
-		
-		<?php echo do_shortcode( $content ); ?> 
+			?></ul><?php 
+		echo do_shortcode( $content ); ?> 
 	</div>
 	<script type="text/javascript">
 	jQuery(document).ready(function($) {
@@ -91,7 +88,12 @@ function olt_display_shortcode_tabs($attr,$content)
 	</script>
 
 	<?php
-	return ob_get_clean();
+	$post_content = ob_get_clean();
+	// var_dump($content, str_replace("\r\n", '',$content));
+	 wp_enqueue_script('jquery');
+     wp_enqueue_script('jquery-ui-core');
+     wp_enqueue_script('jquery-ui-tabs');
+	return str_replace("\r\n", '',$post_content);
 }
 
 
@@ -101,13 +103,11 @@ function olt_tabs_shortcode_init() {
     add_shortcode('tabs', 'olt_display_shortcode_tabs'); // The shell
        
     // Move wpautop to priority 12 (after do_shortcode)
-    //remove_filter('the_content','wpautop',10);
-    //add_filter('the_content','wpautop',12);
+    remove_filter('the_content','wpautop', 10);
+    add_filter('the_content','wpautop', 12);
     
     // add javascript if it is not there yet
-     wp_enqueue_script('jquery');
-     wp_enqueue_script('jquery-ui-core');
-     wp_enqueue_script('jquery-ui-tabs');
+    
 	
 	// pick up styles from the theme or if you have section widget enbles then you are also set. 
 	
